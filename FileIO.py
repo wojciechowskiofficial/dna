92361777eb4e6f5676e369b69806c2b98c292250
adam <adi61044@gmail.com>
2021-03-30 17:27:13 +0200

    add FileIO class, add get_absolute_path and read_file methods in FileIO class

---
+++ FileIO.py
@@ -0,0 +1,27 @@
+import os
+
+class FileIO:
+    '''
+
+    File input output handler
+    '''
+    def __init__(self):
+        pass
+    def get_absolute_path(self, paths: list):
+        '''
+
+        :param paths: list of string each being next path depth starting with the next dir after current working dir
+        :return: operating system invariant absolute path
+        '''
+        absolute_path = os.path.join(str(os.getcwd()), *paths)
+        return absolute_path
+
+    def read_file(self, file_path: str) -> list:
+        '''
+
+        :param file_path: absolute path to read
+        :return: list of oligonucletides
+        '''
+        with open(file_path, 'r') as f:
+            lines = f.read().splitlines()
+        return lines
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from FileIO import FileIO
 
 if __name__ == '__main__':
-    print('bioinf')
+    fileIO = FileIO()
+    lista = list()
+    lista.append('test_instances_dir')
+    lista.append('random_negative_errors_dir')
+    lista.append('9.200-40')
+    fileIO.read_file(fileIO.get_absolute_path(lista))
