0ebd352ae512829546c628c8beb2d7635b718aa4
adam <adi61044@gmail.com>
2021-03-30 20:18:42 +0200

    set identity arcs to -1 for convinience

--- Graph.py
+++ Graph.py
@@ -7,8 +7,8 @@ class Graph:
     Graph handler class.
     Vertices are oligonucleotides
     Arcs are metric values computed between two vertices
-    metric(a, b) = (length of nucleotides - overlap) if overlap is not equal to 0
-                   -1 if there is no overlap
+    metric(a, b) = { (length of nucleotides - overlap) if overlap is not equal to 0
+                   { -1 if there is no overlap
     '''
     def __init__(self):
         self.oligonucleotides_list = list()
@@ -76,3 +76,5 @@ class Graph:
         for row in tqdm(range(self.vertices_no)):
             for col in range(self.vertices_no):
                 self._graph_matrix[row].append(self._compute_metric(self.oligonucleotides_list[row], self.oligonucleotides_list[col]))
+                if row == col:
+                    self._graph_matrix[row][col] = -1
